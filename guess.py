2e8daf85053ee4dbbe8da16d73a3f7694170660a
Sami Jarrar <samijarrar10@gmail.com>
2021-08-29 18:47:41 +0200

    init

---
+++ guess.py
@@ -0,0 +1,47 @@
+
+
+
+
+svar = "SAMI"
+print ("gissa vad jag heter")
+
+print ("Ett tips:") 
+print ()  
+print ("Smakar")
+print ("Amandas")
+print ("Macka")
+print ("Is")
+print ()
+print ()
+gissa =("inget")
+
+while gissa != svar:
+    gissa = input ("vad är mitt namn?").upper ()
+    print ()
+    if gissa != "SAMI":
+        print ("Nej Det är fel.")
+        
+    print ("Det är rätt!")
+
+
+
+
+ 
+ 
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
---
+++ samisfirst.py
@@ -0,0 +1,7 @@
+banan = input("skriv ett ord så skriver jag det 8 gånger")
+
+for count in range(1, 4):
+    produkt = banan * count
+print (produkt)
+
+
